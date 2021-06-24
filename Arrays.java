a53ae0ef3b02509f58881e53b892fc3233feb963
RoseMeyers <80491169+RoseMeyers@users.noreply.github.com>
2021-06-24 18:43:47 -0400

    Add files via upload

--- Arrays.java
+++ Arrays.java
@@ -1,6 +1,17 @@
-package integrationProject2006;
+package integrationproject;
+
+/**
+ * Class "Arrays" will take scanned user input in main and compare it to the
+ *  index in an array in this file that lists the planets.
+
+ * @author Rose Meyers
+ *
+ */
 
 public class Arrays {
-	static String[] planets = {"Earth", "Jupiter", "Venus", "Mars", "Saturn", "Mercury", "Uranus", "Neptune"};
+
+  static String[] planets =
+    {"Earth", "Jupiter", "Venus", "Mars", "Saturn", "Mercury", "Uranus", 
+        "Neptune"};
 
 }
---
+++ Name.java
@@ -0,0 +1,16 @@
+package integrationproject;
+
+/**
+ * Class "Name" will scan user input in main and save as variable here.
+
+ * @author Rose Meyers
+ *
+ */
+
+public class Name {
+  
+  public void greetUser(String userName) {
+    System.out.println("Hello " + userName + "!");
+  }
+
+}
